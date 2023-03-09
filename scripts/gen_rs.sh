25c1a803dcb1f7c56c9d406a857f36fe51448076
KusakabeSi <si@kskb.eu.org>
2023-03-09 01:04:49 +0000

    add md5sum

--- scripts/gen_rs.sh
+++ scripts/gen_rs.sh
@@ -10,10 +10,13 @@ cp /root/arouteserver/scripts/__pycache__/.gitignore /root/arouteserver/cache/.g
 cp /root/arouteserver/clients_rs2.yml /root/arouteserver/clients.yml
 scripts/arouteserver bird -o /root/arouteserver/bird_rs2.conf.out --target-version 2.0.8
 mv /root/arouteserver/bird_rs2.conf.out /root/arouteserver/bird_rs2.conf
+md5sum /root/arouteserver/bird_rs2.conf > /root/arouteserver/bird_rs2.conf.md5
+
 /root/arouteserver/scripts/gen_rs.py 1
 cp /root/arouteserver/clients_rs1.yml /root/arouteserver/clients.yml
 scripts/arouteserver bird -o /root/arouteserver/bird_rs1.conf.out --target-version 2.0.8
 mv /root/arouteserver/bird_rs1.conf.out /root/arouteserver/bird_rs1.conf
+md5sum /root/arouteserver/bird_rs1.conf > /root/arouteserver/bird_rs1.conf.md5
 
 # scripts/arouteserver ixf-member-export --clients /root/arouteserver/clients_rs1.yml "KSKB-IX" 1 | sed "s/fe80::/2404:f4c0:f70e:1980::/g" > /root/gitrs/KSKB-IX/static/files/ix-f.json
 
