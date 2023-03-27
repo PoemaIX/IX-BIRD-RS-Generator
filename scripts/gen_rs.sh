f5f523a7c6f2de7d105d4785156ef67e963d328d
日下部 詩 <github@kskb.eu.org>
2023-03-27 16:08:37 +0800

    Update gen_rs.sh, md5sum use relative

--- scripts/gen_rs.sh
+++ scripts/gen_rs.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 export PYTHONPATH=/root/gitrs/ars
 set -x
-cd /root/gitrs/ars
 set -e
 
 cp /root/arouteserver/scripts/__pycache__/.gitignore /root/arouteserver/cache/.gitignore
 
+cd /root/gitrs/ars
 /root/arouteserver/scripts/gen_rs.py 2
 cp /root/arouteserver/clients_rs2.yml /root/arouteserver/clients.yml
 scripts/arouteserver bird -o /root/arouteserver/bird_rs2.conf.out --target-version 2.0.8
 mv /root/arouteserver/bird_rs2.conf.out /root/arouteserver/bird_rs2.conf
-md5sum /root/arouteserver/bird_rs2.conf > /root/arouteserver/bird_rs2.conf.md5
+cd /root/arouteserver/
+md5sum bird_rs2.conf > /root/arouteserver/bird_rs2.conf.md5
 
+cd /root/gitrs/ars
 /root/arouteserver/scripts/gen_rs.py 1
 cp /root/arouteserver/clients_rs1.yml /root/arouteserver/clients.yml
 scripts/arouteserver bird -o /root/arouteserver/bird_rs1.conf.out --target-version 2.0.8
 mv /root/arouteserver/bird_rs1.conf.out /root/arouteserver/bird_rs1.conf
-md5sum /root/arouteserver/bird_rs1.conf > /root/arouteserver/bird_rs1.conf.md5
+cd /root/arouteserver/
+md5sum bird_rs1.conf > /root/arouteserver/bird_rs1.conf.md5
 
+cd /root/gitrs/ars
 # scripts/arouteserver ixf-member-export --clients /root/arouteserver/clients_rs1.yml "KSKB-IX" 1 | sed "s/fe80::/2404:f4c0:f70e:1980::/g" > /root/gitrs/KSKB-IX/static/files/ix-f.json
 
 
