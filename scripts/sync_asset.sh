c8d9346f8ce7fe11b46bb6e90117f23cb90b2f9e
日下部 詩 <github@kskb.eu.org>
2023-06-08 02:59:38 +0800

    gen_asset_2

--- scripts/sync_asset.sh
+++ scripts/sync_asset.sh
@@ -14,5 +14,5 @@ export CLIENTS_ASSET_PATH="/root/gitrs/KSKB-IX/static/files/kskbix-rs1-estab.yam
 python3 /root/gitrs/RIPE-AS-SET-SYNC/AS-KSKB-IX-RS2.py
 
 export AS_SET="AS-KSKB-IX-RS2"
-export CLIENTS_ASSET_PATH="/root/gitrs/KSKB-IX/static/files/kskbix-rs2-estab.yaml"
-python3 /root/gitrs/RIPE-AS-SET-SYNC/AS-KSKB-IX-RS2.py --flat
+#export CLIENTS_ASSET_PATH="/root/gitrs/KSKB-IX/static/files/kskbix-rs2-estab.yaml"
+python3 /root/arouteserver/scripts/gen_asset_2.py
