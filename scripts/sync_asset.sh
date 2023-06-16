12df6f2bf0a6b5b1544591807d55a3509d802e70
日下部 詩 <github@kskb.eu.org>
2023-06-16 16:32:43 +0800

    Update sync_asset.sh

--- scripts/sync_asset.sh
+++ scripts/sync_asset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set +e
 cd /root/arouteserver/cache
 # export RIPE_PASSWD="XXXXXXXXXXXXXXXXXXXXXXX"
 /root/arouteserver/scripts/gen_ixpf.py | sed "s/fe80::/2404:f4c0:f70e:1980::/g" > /root/gitrs/KSKB-IX/static/files/ix-f.json
@@ -17,3 +17,4 @@ python3 /root/gitrs/RIPE-AS-SET-SYNC/AS-KSKB-IX-RS2.py
 export AS_SET="AS-KSKB-IX-RS2"
 #export CLIENTS_ASSET_PATH="/root/gitrs/KSKB-IX/static/files/kskbix-rs2-estab.yaml"
 python3 /root/arouteserver/scripts/gen_asset_2.py
+set -e
