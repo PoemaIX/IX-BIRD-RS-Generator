44530aba4e35c42442293f10e96050bebf6f9a96
root <root@example.com>
2026-05-03 03:27:29 +0800

    filter-repo at ixpage

--- scripts/sync_ixpage.sh
+++ scripts/sync_ixpage.sh
@@ -13,8 +13,6 @@ git reset --hard origin/main
 /root/arouteserver/scripts/gen_ixpf.py | sed "s/fe80::/2404:f4c0:f70e:1980::/g" > /root/gitrs/KSKB-IX/static/files/ix-f.json
 /root/arouteserver/scripts/gen_member_page.py
 DATE=$(date +'%Y-%m-%d %H:%M:%S')
-GIT_LAST_MSG=$(git log -1 --pretty=%B)
-GIT_DIFF=$(git diff)
 
 build_mkdocs=
 
@@ -33,12 +31,13 @@ if [ "$build_mkdocs" = "1" ]; then
   gzip -f -n -k site/sitemap.xml
 fi
 
+mkdir -p /tmp/ixpage_bak
+cp -r site /tmp/ixpage_bak/ 2>/dev/null || true
+pip3 install git-filter-repo
+git filter-repo --invert-paths --path site/ --force
+cp -r /tmp/ixpage_bak/site . 2>/dev/null || true
+rm -rf /tmp/ixpage_bak
+
 git add -A
-if  [[ $GIT_LAST_MSG == "sync members at"* ]] ;
-then
-    git diff-index --quiet HEAD || git commit -m "sync members at $DATE" --amend
-    #git commit -m "sync members at $DATE"
-else
-    git diff-index --quiet HEAD || git commit -m "sync members at $DATE"
-fi
-git push -f
+git diff-index --quiet HEAD || git commit -m "sync members at $DATE"
+git push -f git@github.com:PoemaIX/KSKB-IX.git HEAD:main
